4b90eabb212d6f9c9da63b8322ec73d2261e1fff
Jose Alzate <malzatej257@gmail.com>
2025-05-12 13:45:29 -0500

    Modificacion de index principal JoseAlzate

--- index.js
+++ index.js
@@ -2,8 +2,10 @@ function main(params) {
     console.log("Hello, world!");
 }
 
+function hello(params) {
+    console.log("Hello, Jose!");
+}
 
 function valentino(params) {
     console.log("Hola, Valentino!");
 }
-
